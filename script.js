197309e6050ab4d4ce858a8bb33a10e1a4bf7bef
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-28 21:54:00 +0200

    add last responsiveness

--- script.js
+++
@@ -1,7 +0,0 @@
-
- let menu = document.querySelector(".list-of-items")
-
- menu.addEventListener("click",()=>{
-    let list= document.querySelector(".btn-1-1")
-    list.classList.toggle("hidden")
- })
