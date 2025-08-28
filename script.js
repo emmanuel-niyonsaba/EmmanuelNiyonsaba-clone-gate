7906783d9fab09a415273798d31c91be5ea6a78b
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-28 21:47:17 +0200

    add : humbaga menu

---
+++ script.js
@@ -0,0 +1,7 @@
+
+ let menu = document.querySelector(".list-of-items")
+
+ menu.addEventListener("click",()=>{
+    let list= document.querySelector(".btn-1-1")
+    list.classList.toggle("hidden")
+ })
